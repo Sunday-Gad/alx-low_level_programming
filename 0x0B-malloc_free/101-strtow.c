b76a18faeed5deb6c2c09fbf1ba1eaeece7daec5
Sunday-Gad <sundayzephaniahgad@gmail.com>
2022-07-25 19:48:17 +0000

    a function that splits a string into words.

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -51,7 +51,7 @@ char **strtow(char *str)
 			n = 0;
 			while (str[n] != ' ')
 				n++;
-			strDup[i] = malloc(sizeof(char) * (1));
+			strDup[i] = malloc(sizeof(char) * (n + 3));
 			if (strDup[i] == NULL)
 			{
 				while (--i >= 0)
